9347b0d298b7fbffb61b19c91fb1c79c2059c954
Sheryll Dumapal <sheryllhhs@outlook.com>
2023-12-14 22:06:58 +0100

    save

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+const navIcons = [
+  { src: "/assets/icons/search.svg", alt: "search" },
+  { src: "/assets/icons/search.svg", alt: "search" },
+  { src: "/assets/icons/search.svg", alt: "search" },
+];
 
-function Navbar() {
-  return <div>Navbar</div>;
-}
+const Navbar = () => {
+  return (
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center-center gap-1">
+          <Image
+            src="/assets/icons/dataxlogo.png"
+            width={100}
+            height={100}
+            alt="logo"
+          />
+
+          <p className="nav-logo">
+            Price <span className="text-primary">Wise</span>
+          </p>
+          <div className="flex items-center gap 5"></div>
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
